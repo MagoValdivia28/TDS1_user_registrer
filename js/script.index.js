90eafdf776b5e976d588fa105e103ce3a4985234
felipe pedro <felipepedro.senai564@gmail.com>
2023-08-21 09:18:39 -0300

    feat: creat showUsers and showRegisters fuctions

---
+++ js/script.index.js
@@ -0,0 +1,10 @@
+function showUsers(){
+    document.getElementById("main-div").classList.add("hidden")
+    document.getElementById("sub-div").classList.remove("hidden")
+}
+
+function showRegister(){
+    document.getElementById("main-div").classList.remove("hidden");
+    document.getElementById("sub-div").classList.add("hidden")
+}
+
