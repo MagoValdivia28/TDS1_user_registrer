aba527cc39c3b162731a9dcd4e5c854d58a9248b
felipe pedro <felipepedro.senai564@gmail.com>
2023-08-21 09:19:17 -0300

    feat: creat class user and userList

--- js/script.index.js
+++ js/script.index.js
@@ -7,4 +7,23 @@ function showRegister(){
     document.getElementById("main-div").classList.remove("hidden");
     document.getElementById("sub-div").classList.add("hidden")
 }
+class User{
+    constructor(name, email, birthdate, age, city, telephone, cpf, client){
+        this.name = name
+        this.email = email
+        this.birthdate = birthdate 
+        this.age = age
+        this.city = city
+        this.telephone = telephone
+        this.cpf = cpf
+        this.client = client
+    }  
+}
+
+class UserList{
+    constructor(){
+        this.users = [];
+    }
+}
+
 
